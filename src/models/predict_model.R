fc0bb81e6dd1d9e0317c6e3f6d2d2820c17bbd02
Caroline Mimeault <carolinemimeault@gmail.com>
2019-03-24 19:54:30 -0400

    add features to random forest model

--- src/models/predict_model.R
+++ src/models/predict_model.R
@@ -15,7 +15,14 @@ data <- data %>%
     reanalysis_min_air_temp_k,
     station_min_temp_c,
     reanalysis_tdtr_k,
-    station_diur_temp_rng_c
+    station_diur_temp_rng_c,
+    ndvi_ne,
+    ndvi_nw,
+    ndvi_se,
+    ndvi_sw,
+    reanalysis_max_air_temp_k,
+    reanalysis_relative_humidity_percent,
+    reanalysis_avg_temp_k
   ) %>%
   mutate_if(is.character, as.factor)
 
--- src/models/train_model.R
+++ src/models/train_model.R
@@ -2,21 +2,30 @@ library(tidyverse)
 library(randomForest)
 library(here)
 
-training <- read_csv(here("data", "processed", "clean_train_data.csv"))
+training <- read_csv(here("data", "processed", "clean_training_data.csv"))
 
 set.seed(100)
 
 train <- training %>%
- select(city,
-        year,
-        weekofyear,
-        total_cases,
-        reanalysis_air_temp_k,
-        reanalysis_min_air_temp_k, 
-        station_min_temp_c,
-        reanalysis_tdtr_k,
-        station_diur_temp_rng_c) %>%
- mutate_if(is.character, as.factor)
+  select(
+    city,
+    year,
+    weekofyear,
+    total_cases,
+    reanalysis_air_temp_k,
+    reanalysis_min_air_temp_k,
+    station_min_temp_c,
+    reanalysis_tdtr_k,
+    station_diur_temp_rng_c,
+    ndvi_ne,
+    ndvi_nw,
+    ndvi_se,
+    ndvi_sw,
+    reanalysis_max_air_temp_k,
+    reanalysis_relative_humidity_percent,
+    reanalysis_avg_temp_k
+  ) %>%
+  mutate_if(is.character, as.factor)
 
 
 model <- randomForest(total_cases ~ ., data = train, ntree = 500, mtry = 6, importance = TRUE)
