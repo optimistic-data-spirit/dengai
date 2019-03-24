f108561018dc5ff784e31a0951868047fb0080da
Caroline Mimeault <carolinemimeault@gmail.com>
2019-03-24 15:16:28 -0400

    change the name of merged datasets csv

--- src/data/merge_datasets.R
+++ src/data/merge_datasets.R
@@ -6,6 +6,6 @@ remove(list = ls()) # clear workspace memory
 labels <- read_csv(here("data", "raw", "dengue_labels_train.csv"))
 features <- read_csv(here("data", "raw", "dengue_features_train.csv"))
 
-training <- merge(labels, features, by = c("city", "year", "weekofyear"))
+merged_dataset <- merge(labels, features, by = c("city", "year", "weekofyear"))
 
-write_csv(training, path = here("data", "interim", "training.csv"))
+write_csv(merged_dataset, path = here("data", "interim", "merged_dataset.csv"))
