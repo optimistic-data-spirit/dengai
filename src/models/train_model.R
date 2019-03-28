66bedb54682552a5d4eb408303879240301ad9e4
Caroline Mimeault <carolinemimeault@gmail.com>
2019-03-27 21:28:32 -0400

    random forest with all features

--- src/models/train_model.R
+++ src/models/train_model.R
@@ -12,19 +12,27 @@ train <- training %>%
     year,
     weekofyear,
     total_cases,
-    reanalysis_air_temp_k,
-    reanalysis_min_air_temp_k,
-    station_min_temp_c,
-    reanalysis_tdtr_k,
-    station_diur_temp_rng_c,
+    week_start_date,
     ndvi_ne,
     ndvi_nw,
     ndvi_se,
     ndvi_sw,
+    precipitation_amt_mm,
+    reanalysis_air_temp_k,
+    reanalysis_avg_temp_k,
+    reanalysis_dew_point_temp_k,
     reanalysis_max_air_temp_k,
+    reanalysis_min_air_temp_k,
+    reanalysis_precip_amt_kg_per_m2,
     reanalysis_relative_humidity_percent,
-    reanalysis_avg_temp_k
-  ) %>%
+    reanalysis_specific_humidity_g_per_kg,
+    reanalysis_tdtr_k,
+    station_avg_temp_c,
+    station_diur_temp_rng_c,
+    station_max_temp_c,
+    station_min_temp_c,
+    station_precip_mm
+      ) %>%
   mutate_if(is.character, as.factor)
 
 
